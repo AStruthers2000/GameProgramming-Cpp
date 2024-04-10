846c87b84994386dc2b697886993cfc1d79586e9
AStruthers2000 <struthers919@gmail.com>
2024-04-09 22:06:09 -0700

    Added multiball project and removed other Pong example that I have no memory of creating....

---
+++ Code/Multiball/Multiball/Multiball.cpp
@@ -0,0 +1,16 @@
+﻿// Multiball.cpp : Defines the entry point for the application.
+//
+
+#include "Game.h"
+
+int main(int argc, char* argv[])
+{
+	Game game;
+    bool success = game.Initialize();
+    if(success)
+    {
+        game.RunLoop();
+    }
+    game.Shutdown();
+	return 0;
+}
